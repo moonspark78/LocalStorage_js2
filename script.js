9c8b5637190b19bce120ce24136d2b2d21a10cb2
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-04 16:32:32 +0100

    working script

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const addButton = document.getElementById('addTask');
 const inputTask = document.getElementById('inputTask');
 const taskList = document.getElementById('taskList');
+
+addButton.addEventListener('click', addTask);
+
+function addTask(){
+    const task = inputTask.value.trim();
+    if(task === ""){
+        alert('Please enter a task');
+        return;
+    }
+    const taskItem = document.createElement('li');
+    taskItem.textContent = task;
+    taskList.appendChild(taskItem);
+    inputTask.value = "";
+};
