2e335275bc449798a0493f7c35574d8ecd23bd59
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-04 19:20:38 +0100

    working script js

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const addButton = document.getElementById('addTask');
 const inputTask = document.getElementById('inputTask');
 const taskList = document.getElementById('taskList');
 
+let tasks = [];
+
+
+
+window.onload = function(){
+    const storedTasks = localStorage.getItem('tasks');
+    if(storedTasks){
+        tasks = JSON.parse(storedTasks);
+        tasks.forEach(task => {
+            const taskItem = document.createElement('li');
+            taskItem.textContent = task;
+            taskList.appendChild(taskItem);
+        });
+
+    }
+};
+
 addButton.addEventListener('click', addTask);
 
 function addTask(){
@@ -10,6 +27,9 @@ function addTask(){
         alert('Please enter a task');
         return;
     }
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
     const taskItem = document.createElement('li');
     taskItem.textContent = task;
     taskList.appendChild(taskItem);
