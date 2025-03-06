6c63289c9e755d450ee739861e5d6e6f3a8f7797
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-06 07:54:48 +0100

    v2 start : working on  js  :art:

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const addButton = document.getElementById('add');
 const inputTask = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
+
+let tasks = [];
+
+function createTaskElement(task) {
+    const taskItem = document.createElement('li');
+    taskItem.innerText = task;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = '🗑️';
+};
